9e643aa3e57717966cc80b16858f05d48c71b154
Yuto Nishida <yuyu.nishida@gmail.com>
2017-06-22 16:04:53 -0400

    improve ui
    Clarify meaning of output

--- ChemTools/main.cpp
+++ ChemTools/main.cpp
@@ -16,13 +16,18 @@ using namespace std;
 int main() {
 	map<string, Element> periodicTable = Equation::elementParse("chemical_elements.csv");
 	
+	cout << "Enter chemical equation: " << endl;
 	string s;
 	cin >> s;
+	cout << "Enter mass in grams of 1st term of output: " << endl;
 	double input;	//desired mass of first compound of the products
 	cin >> input;
 
+	cout << endl;
+	cout << "Mass of each precursor, ignore coefficients: " << endl;
 	Equation eq(periodicTable, s);
 	eq.printPreAmt(input);
+	cout << endl;
 
 	return 0;
 }
